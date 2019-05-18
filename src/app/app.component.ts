399b2eed5ffcba1cbe5eddb3308d9ae32154f1fb
wholzapfel <werner.holzapfel@trivento.nl>
2019-05-18 10:19:30 +0200

    setup CI/CD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,67 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {Platform} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+    selector: 'app-root',
+    templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  public appPages = [
-    {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
-    {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
+    public appPages = [
+        {
+            title: 'Home',
+            url: '/home',
+            icon: 'home'
+        },
+        {
+            title: 'Totaalstand',
+            url: '/list',
+            icon: 'list'
+        }, {
+            title: 'Teamstand',
+            url: '/stand',
+            icon: 'list'
+        }, {
+            title: 'Wedstrijdenstand',
+            url: '/stand',
+            icon: 'list'
+        }, {
+            title: 'Vragenstand',
+            url: '/stand',
+            icon: 'list'
+        }, {
+            title: 'Competitiestand',
+            url: '/stand',
+            icon: 'list'
+        }, {
+            title: 'Punten per voetballer',
+            url: '/stand',
+            icon: 'list'
+        }, {
+            title: 'Gekozen voetballers',
+            url: '/stand',
+            icon: 'list'
+        }, {
+            title: 'Deelnemers',
+            url: '/stand',
+            icon: 'list'
+        }
+    ];
+
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar
+    ) {
+        this.initializeApp();
     }
-  ];
 
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+        });
+    }
 }
