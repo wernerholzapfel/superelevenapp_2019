f177d6f6e7d5596e2bc75bde9ee5ee0e92182584
wernerholzapfel <werner.holzapfel@trivento.nl>
2020-10-09 16:12:40 +0200

    prepartions for teamplayers

--- src/app/preperations_tab/players/players.page.ts
+++ src/app/preperations_tab/players/players.page.ts
@@ -26,52 +26,13 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
 
 export class PlayersPage implements OnInit, OnDestroy {
 
-    clubs = [{
-        name: 'Ajax',
-        formation: [{
-            type: 'Attacker',
-            players: []
-        }, {
-            type: 'Midfielder',
-            players: []
-        }, {
-            type: 'Defender',
-            players: []
-        }, {
-            type: 'Goalkeeper',
-            players: []
-        }]
-    }, {
-        name: 'PSV',
-        formation: [{
-            type: 'Attacker',
-            players: []
-        }, {
-            type: 'Midfielder',
-            players: []
-        }, {
-            type: 'Defender',
-            players: []
-        }, {
-            type: 'Goalkeeper',
-            players: []
-        }]
-    }];
-
-    spelers = [
-        'Dolberg',
-        'Lozana',
-        'Take a shower',
-        'Check e-mail',
-    ];
-
     competition: any;
     prediction: any;
-    formation: Formation[];
     players: Teamplayer[];
-    teams: Team[];
-    team: FormationPlayer[] = []; // todo check typedefintion
     unsubscribe = new Subject<void>();
+    customPopoverOptions: any = {
+        header: 'Positie',
+    };
 
     constructor(private store: Store<IAppState>,
                 private modalController: ModalController,
@@ -91,58 +52,18 @@ export class PlayersPage implements OnInit, OnDestroy {
                         this.competition = competition;
                         this.prediction = competition.predictions.find(p => p.predictionType === PredictionType.Team);
                         return combineLatest([
-                            this.playerService.getPlayersByPredictionId(this.prediction.id).pipe(take(1)),
-                            this.formationService.getFormation().pipe(take(1)),
-                            this.teamService.getTeams().pipe(take(1)),
-                            this.predictionService.getTeamPrediction(this.prediction.id).pipe(take(1))]);
+                            this.playerService.getPlayersByPredictionId(this.prediction.id).pipe(take(1))])
                     } else {
                         return from([]);
                     }
                 }))
             .pipe(takeUntil(this.unsubscribe))
             .subscribe(
-                ([players, formation, teams, predictionTeam]) => {
-                    if (players && formation && teams) {
-                        this.formation = formation;
+                ([players]) => {
+                    if (players) {
                         this.players = players;
-                        this.teams = teams;
-
-                        // predictionTeam doorlopen en toevoegen aan juiste formationline positie
-                        predictionTeam.map(teamPlayer => {
-                            this.formation.find(f => {
-                                return f.position === teamPlayer.teamPlayer.position &&
-                                    f.players.filter(ftp => !ftp.selected).length > 0;
-                            }).players.filter(ftp => !ftp.selected).map((player, index) => {
-                                return index === 0 ? Object.assign(player,
-                                    {id: teamPlayer.teamPlayer.id},
-                                    {player: teamPlayer.teamPlayer.player},
-                                    {team: teamPlayer.teamPlayer.team},
-                                    {selected: true}) :
-                                    player;
-                            });
-                        });
-
                     }
                 });
-    }
-
-
-    drop(event: CdkDragDrop<string[]>) {
-        if (event.previousContainer === event.container) {
-            moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-        } else {
-            transferArrayItem(event.previousContainer.data,
-                event.container.data,
-                event.previousIndex,
-                event.currentIndex);
-        }
-    }
-
-    save() {
-        const blaat: any[] = this.clubs.map(club => club.formation.map(formation => formation.players.map(
-            player => Object.assign({player}, {position: formation.type}))));
-
-        console.log(this.flattenDeep(blaat));
     }
 
     ngOnDestroy(): void {
@@ -150,8 +71,18 @@ export class PlayersPage implements OnInit, OnDestroy {
         this.unsubscribe.unsubscribe();
     }
 
-    flattenDeep(arr1) {
-        return arr1.reduce((acc, val) => Array.isArray(val) ? acc.concat(this.flattenDeep(val)) : acc.concat(val), []);
+    savePositie(event, player) {
+        this.playerService.updatePlayer({...player, position: event.detail.value})
+            .subscribe(updatedPlayer => {
+                this.players = this.players.map(p => {
+                    return p.id === player.id ? {...player, position: event.detail.value} : p
+                });
+            });
     }
 
+    setPlayerInActive(player) {
+        this.playerService.updatePlayer({...player, active: false}).subscribe(updatedPlayer => {
+            this.players = this.players.filter(p => p.id !== player.id);
+        });
+    }
 }
--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -3,7 +3,8 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {RankingTeam} from '../models/prediction.model';
 import {environment} from '../../environments/environment';
-import {TeamplayerResponse} from '../models/teamplayer.model';
+import {Teamplayer, TeamplayerResponse} from '../models/teamplayer.model';
+import {Team} from '../models/team.model';
 
 @Injectable({
     providedIn: 'root'
@@ -15,6 +16,11 @@ export class PlayerService {
 
     getPlayersByPredictionId(predictionId: string): Observable<RankingTeam[]> {
         return this.http.get<RankingTeam[]>(`${environment.apiBaseUrl}/team-player/prediction/${predictionId}`);
+    }
+
+
+    updatePlayer(player: Teamplayer): Observable<Teamplayer> {
+        return this.http.put<Teamplayer>(`${environment.apiBaseUrl}/team-player/${player.id}`, player)
     }
 
     getPlayersScore(predictionId: string, roundId: string): Observable<TeamplayerResponse[]> {
